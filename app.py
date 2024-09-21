63ae306b02298f63355cdb9a0f5ef050f19d9505
xxxjer <jerlynchua@gmail.com>
2024-09-21 06:54:24 +0000

    1

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask,render_template,request
+
 app = Flask(__name__)
-@app.route("/",methods = ["GET","POST"])
-# GET = We are now in backend. Get information from frontend 
-# POST = Retrieve information from backend to frontend
-# Direction will change depending on whether we are in backend or frontend
-# @ is like a decorator. With @, the code app.route will run first before render template
+
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/prediction_DBS",methods=["GET","POST"])
+def prediction_DBS():
+    return(render_template("prediction_DBS.html"))
+
+@app.route("/prediction_result_DBS",methods=["GET","POST"])
+def prediction_result_DBS():
+    q = float(request.form.get("q"))
+    r = (-50.6 * q) + 90.2
+    return(render_template("prediction_result_DBS.html",r=r))
 
 if __name__ == "__main__":
     app.run()
